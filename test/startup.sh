4e7203f6e6b826856d07205770347795c36b89e5
sc140f <sc140f@att.com>
2019-08-04 15:22:03 -0500

    adding consul leader detection to startup + some readme updates

--- test/startup.sh
+++ test/startup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-#at some point I need to plug in logic to query consul rather than just waiting
-sleep 45
+echo "Waiting for consul leader election..."
+while ! curl --request GET --url http://consul:8500/v1/status/leader | grep -q :8300; do
+ 	sleep 1
+done
+echo "...good to go"
 node node_modules/node-red/red.js --userDir /data --settings /home/node-red/settings.js -v
