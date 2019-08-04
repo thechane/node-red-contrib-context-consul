f206787b22502a0a48271394881fbc7b11edc4f8
sc140f <sc140f@att.com>
2019-08-04 15:49:02 -0500

    added a demo flow showing how the context store can be used

--- test/settings.js
+++ test/settings.js
@@ -66,6 +66,7 @@ module.exports = {
 
     // The file containing the flows. If not set, it defaults to flows_<hostname>.json
     //flowFile: 'flows.json',
+    flowFile: '/home/node-red/flows.json',
 
     // To enabled pretty-printing of the flow within the flow file, set the following
     //  property to true:
